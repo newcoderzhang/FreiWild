b89221ea0033c1ed077b4ab03cfed06e24ff2a80
Paul Ovchinnikov <ovchinnikov@softomate.com>
2015-07-29 00:24:00 +0600

    Convert response list into Observable<MyItem>

--- app/src/main/java/com/accord/android/freiwild/app/MainActivity.java
+++ app/src/main/java/com/accord/android/freiwild/app/MainActivity.java
@@ -1,18 +1,26 @@
 package com.accord.android.freiwild.app;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
 
 import com.google.gson.GsonBuilder;
 
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import butterknife.ButterKnife;
 import butterknife.OnClick;
 import retrofit.RestAdapter;
 import retrofit.converter.GsonConverter;
+import rx.Observable;
+import rx.Subscription;
 import rx.android.schedulers.AndroidSchedulers;
 import rx.functions.Action1;
+import rx.functions.Func1;
 import rx.schedulers.Schedulers;
 
 
@@ -43,14 +51,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void performRequest() {
-        requestsApi.getReleases()
+        getReleases(requestsApi)
                 .subscribeOn(Schedulers.newThread())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Action1<List<Release>>() {
+                .subscribe(new Action1<Release>() {
                     @Override
-                    public void call(List<Release> releases) {
-                        releases.size();
+                    public void call(Release release) {
+                        Log.d(MainActivity.class.getSimpleName(), release.title);
                     }
                 });
+    }
+
+    private Observable<Release> getReleases(RequestsApi requestsApi) {
+        return requestsApi.getReleases()
+                .timeout(5, TimeUnit.SECONDS)
+                .retry(2)
+                .onErrorResumeNext(handleError())
+                .flatMap(convertListToItems());
+    }
+
+//    private final Subscription subscription =
+//            requestsApi.getReleases()
+//                    .timeout(5, TimeUnit.SECONDS)
+//                    .retry(2)
+//                    .onErrorResumeNext(handleError())
+//                    .flatMap(convertListToItems() )
+//                    .subscribe();
+
+    public Func1<Throwable, Observable<? extends List<Release>>> handleError() {
+        return new Func1<Throwable, Observable<? extends List<Release>>>() {
+            @Override
+            public Observable<? extends List<Release>> call(Throwable throwable) {
+                Log.e(MainActivity.class.getSimpleName(), throwable.getMessage());
+                return Observable.<List<Release>>empty();
+            }
+        };
+    }
+
+    public Func1<List<Release>, Observable<Release>> convertListToItems() {
+        return new Func1<List<Release>, Observable<Release>>() {
+            @Override
+            public Observable<Release> call(List<Release> releases) {
+                return Observable.from(releases);
+            }
+        };
     }
 }
