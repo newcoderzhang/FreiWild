802fa4ad253b248dcaa0bb62d786671937bef560
Paul <ovchinnikov@softomate.com>
2015-08-25 16:16:43 +0600

    Releases layout fixes

--- app/src/main/java/com/accord/android/freiwild/app/ui/ReleasesAllFragment.java
+++ app/src/main/java/com/accord/android/freiwild/app/ui/ReleasesAllFragment.java
@@ -6,7 +6,6 @@ import android.graphics.Rect;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
-import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -131,7 +130,7 @@ public class ReleasesAllFragment extends Fragment {
 
         @Override
         public ReleaseHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-            View itemView = mInflater.inflate(R.layout.item_release_card, parent, false);
+            View itemView = mInflater.inflate(R.layout.item_release_card_vertical, parent, false);
             return new ReleaseHolder(itemView);
         }
 
